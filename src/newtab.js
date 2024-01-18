a47c4265bce7f9244ce6da40e9650a67cfc747a7
alvyynm <alvyynm@gmail.com>
2024-01-18 09:34:48 -0300

    style: format code with prettier

--- src/newtab.js
+++ src/newtab.js
@@ -1,18 +1,18 @@
 // Redirect to chrome://dino when a new tab is opened.
-const url = "chrome://dino";
-chrome.tabs.create({ url, active: true });
+const url = 'chrome://dino'
+chrome.tabs.create({ url, active: true })
 
 // remove the first opened blank tab in the active window
 chrome.tabs.query({ currentWindow: true }, function (tabs) {
-  // get the second last tab in the array
-  const secondLastTab = tabs[tabs.length - 2];
+	// get the second last tab in the array
+	const secondLastTab = tabs[tabs.length - 2]
 
-  // get the tab ID of the second last tab
-  const secondLastTabId = secondLastTab.id;
+	// get the tab ID of the second last tab
+	const secondLastTabId = secondLastTab.id
 
-  // close the tab with the given tab ID
-  chrome.tabs.remove(secondLastTabId, function () {
-    // this function will be called after the tab is closed
-    console.log("Tab closed");
-  });
-});
+	// close the tab with the given tab ID
+	chrome.tabs.remove(secondLastTabId, function () {
+		// this function will be called after the tab is closed
+		console.log('Tab closed')
+	})
+})
